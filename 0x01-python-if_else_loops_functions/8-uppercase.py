364355ebd71add56f035f19b49a656e5a0be4579
Uceetheblackone <anyanwuuchechuwkwujay@gmail.com>
2023-05-09 15:02:19 -0700

    11-pow.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(s):
     for c in s:
-        if ord('a') <= ord(c) <= ord('z'):
-            uppercase_c = chr(ord(c) - 32)
-        else:
-            uppercase_c = c
-            print("{}".format(uppercase_c), end="")
-            print()
+         if ord('a') <= ord(c) <= ord('z'):
+             uppercase_c = chr(ord(c) - 32)
+         else:
+             uppercase_c = c
+             print("{}".format(uppercase_c), end="")
+             print()

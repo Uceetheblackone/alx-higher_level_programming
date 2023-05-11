4e9a7140580a2deb121d535fbc5658f33a750428
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-11 13:58:33 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
-""" program that imports the function def add(a, b): from the file add_0.py
-prints the result of the addition 1 + 2 = 3
-"""
 if __name__ == "__main__":
+    """ program that imports the function def add(a, b): from the file add_0.py
+    prints the result of the addition 1 + 2 = 3
+    """
     from add_0 import add
     a = 1
     b = 2

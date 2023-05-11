1b18542cf305d6ed0f2cbc15c1bddb6e7a8ea77b
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-11 13:53:01 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,7 @@
 """ program that imports the function def add(a, b): from the file add_0.py
 prints the result of the addition 1 + 2 = 3
 """
+if __name__ == "__main__":
 from add_0 import add
 a = 1
 b = 2

1ce8c7c4a44d2424e1a4f567147b5c0d7827942c
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-11 13:56:05 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,7 @@
 prints the result of the addition 1 + 2 = 3
 """
 if __name__ == "__main__":
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))

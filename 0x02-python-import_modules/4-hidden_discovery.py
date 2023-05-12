7942e6b4b9d55bc7f9ba289dcbf8db9a4884c748
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-11 17:14:57 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 mod:wule."""
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
     for name in dir(hidden_4):
         if not name.startswith('__'):
             print(name)
-

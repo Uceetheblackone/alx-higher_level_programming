3f18ffc375e0a784e718667593509ddcd3e0eaa7
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-15 09:18:12 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """ prints a matrix of integers."""
+    for row in matrix:
+        for item in row:
+            print("{:d}".format(item), end=" ")
+        print()

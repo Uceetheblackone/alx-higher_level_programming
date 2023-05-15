23d5f20306d6ccb89b9274f26f5a323551285ae0
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-15 11:11:19 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """ finds the biggest integer of a list."""
+    if my_list == "":
+        return None
+    maximum_number = my_list[0]
+    for index in my_list:
+        if index > maximum_number:
+            maximum_number = index
+    return maximum_number

6d98fdf3b10f8945d0e9a896d281e89df267f4e8
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-17 16:31:57 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,15 +13,7 @@ def roman_to_int(roman_string):
         return 0
     if not roman_string:
         return 0
-    roman_num = {
-            'I' : 1,
-            'V' : 5,
-            'X' : 10,
-            'L' : 50,
-            'C' : 100,
-            'D' : 500,
-            'M' : 1000
-            }
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(roman_num.keys())
     result = 0
     prev_roman_num = 0

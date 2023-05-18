a1a487dc0b8a469d6fd0ddf53c03794dca887765
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-17 17:01:49 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
-def subtract_sum(list_num):
-    max_val = max(list_num)
+def to_sub_max(list_num):
     to_subtract = 0
+    max_val = max(list_num)
 
-    for a in list_num:
-        if max_val > a:
-            to_subtract += a
+    for j in list_num:
+        if max_val > j:
+            to_subtract += j
+
     return (max_val - to_subtract)
 
 
 def roman_to_int(roman_string):
-    """converts a Roman numeral to an integer."""
-    if not isinstance(roman_string, str):
-        return 0
     if not roman_string:
         return 0
 
+    if not isinstance(roman_string, str):
+        return 0
 
-    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(roman_num.keys())
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_num.keys())
 
-
-    result = 0
-    prev_roman_num = 0
+    no = 0
+    prev_rom = 0
     list_num = [0]
 
-    for index in roman_string:
-        for roman_numeral in list_keys:
-            if roman_numeral == index:
-                if roman_num.get(index)  <= prev_roman_num:
-                    result += subtract_sum(list_num)
-                    list_num = [roman_num.get(index)]
-        else:
-            list_num.append(roman_num.get(index))
-        prev_roman_num = roman_num.get(index)
-    result += subtract_sum(list_num)    
+    for i in roman_string:
+        for roman_num in list_keys:
+            if roman_num == i:
+                if rom_num.get(i) <= prev_rom:
+                    no += to_sub_max(list_num)
+                    list_num = [rom_num.get(i)]
+                else:
+                    list_num.append(rom_num.get(i))
 
-    return (result)
+                prev_rom = rom_num.get(i)
+
+    no += to_sub_max(list_num)
+
+    return (no)

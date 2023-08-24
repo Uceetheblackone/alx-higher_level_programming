9b0b847ebdb2fc570ff47332e18f2a3bd5fd4a7f
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-08-23 19:43:59 -0700

    update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-"""
-Lists all State objects and corresponding City objects
-from the database hbtn_0e_101_usa
-"""
-
+'''Prints all State objects and their City objects in a database.
+'''
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
+
+from relationship_state import Base, State
 from relationship_city import City
 
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Create a connection to the database
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            username, password, db_name
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(
+            user, pword, db_name
         )
-    )
-
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query for all State objects with their cities relationship
-    states = session.query(State).order_by(State.id).all()
-
-    # Print the results
-    for state in states:
-        print('{}: {}'.format(state.id, state.name))
-        for city in state.cities:
-            print('\t{}: {}'.format(city.id, city.name))
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).order_by(State.id.asc()).all()
+        for state in result:
+            print('{}: {}'.format(state.id, state.name))
+            for city in state.cities:
+                print('\t{}: {}'.format(city.id, city.name))
+        session.close()
